d7050cdd5163b25b4b5461c58df983d953de17e2
Luigi Di Benedetto <ing.luigidibenedetto@gmail.com>
2021-02-15 13:40:01 +0100

    commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# FlashWeatherApp
-A small web app to display the wheather for the current day from any location or city around the world
--- index.js
+++ index.js
@@ -1,187 +1,217 @@
+
 /**
-Layout
+ * Layout
 E necessario creare una input text HTML che permetta di digitare il nome di una città. L’API
 va richiamata al click su un pulsante posto accanto all’input di text. Quando si atterra sulla
 pagina, prevedere una visualizzazione a “card” del meteo di queste 4 città: Roma, Londra,
 Madrid, New York.
-*/
+ * 
+ */
 
 const state = {
-  config: {
-    api_key: "81abe82ed06ba133fa0b2c72305a7650",
-    base_url: "https://api.themoviedb.org/3",
-    images: null
-  },
-  movies: null
-};
-
-/**
- * Utilities
- */
-function getUrl(pathName) {
-  const { api_key, base_url } = state.config;
-
-  return `${base_url}${pathName}?api_key=${api_key}`;
+  cities: null
 }
 
-function getImageUrl(imgPath) {
-  // const { secure_base_url, backdrop_sizes } = state.config.images
 
-  const secure_base_url = state.config.images.secure_base_url;
-  const backdrop_sizes = state.config.images.backdrop_sizes;
+function call() {
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=3a6190d85f31d51b97e9a8f55d3a2aaa', {
+      method: 'GET'
+  })
+      .then( (response) => {
+          console.log('risposta fetch:', response);
 
-  return `${secure_base_url}${backdrop_sizes[0]}${imgPath}`;
+          return response.json();
+
+      })
+     .then((dataJson) => {
+          
+          state.cities = dataJson;
+          console.log('state.cities: ', state.cities);
+             
+          
+      });
+
+  
 }
+document.addEventListener("DOMContentLoaded", call);
 
-async function getData(url) {
-  try {
-    const response = await fetch(url);
-    const result = await response.json();
 
-    if (!response.ok) {
-      throw result;
-    }
-    return result;
-  } catch (errorMessage) {
-    console.log(errorMessage);
-  }
-}
+// const state = {
+//   config: {
+//     api_key: "3a6190d85f31d51b97e9a8f55d3a2aaa",
+//     base_url: "api.openweathermap.org/data/2.5/weather?q=London&appid=",
+//     images: null
+//   },
+//   movies: null
+// };
 
-/**
- * Actions per caricare i dati
+// /**
+//  * Utilities
+//  */
+// function getUrl(pathName) {
+//   const { api_key, base_url } = state.config;
+
+//   return `${base_url}${pathName}?api_key=${api_key}`;
+// }
+
+// function getImageUrl(imgPath) {
+//   // const { secure_base_url, backdrop_sizes } = state.config.images
+
+//   const secure_base_url = state.config.images.secure_base_url;
+//   const backdrop_sizes = state.config.images.backdrop_sizes;
+
+//   return `${secure_base_url}${backdrop_sizes[0]}${imgPath}`;
+// }
+
+// async function getData(url) {
+//   try {
+//     const response = await fetch(url);
+//     const result = await response.json();
+
+//     if (!response.ok) {
+//       throw result;
+//     }
+//     return result;
+//   } catch (errorMessage) {
+//     console.log(errorMessage);
+//   }
+// }
+
+// /**
+//  * Actions per caricare i dati
  
- * ottiene i la lista di film più popolari
- *
- * @link https://developers.themoviedb.org/3/movies/get-popular-movies
- * 
- * top rated:
- * https://developers.themoviedb.org/3/movies/get-top-rated-movies
- * 
- * pop series:
- * https://developers.themoviedb.org/3/tv/get-popular-tv-shows
- */
-async function getPopularMovies() {
-  const popularMoviesURL = getUrl("/movie/popular");
+//  * ottiene i la lista di film più popolari
+//  *
+//  * @link https://developers.themoviedb.org/3/movies/get-popular-movies
+//  * 
+//  * top rated:
+//  * https://developers.themoviedb.org/3/movies/get-top-rated-movies
+//  * 
+//  * pop series:
+//  * https://developers.themoviedb.org/3/tv/get-popular-tv-shows
+//  */
+// async function getPopularMovies() {
+//   const popularMoviesURL = getUrl("/movie/popular");
 
-  const rawResponse = await getData(popularMoviesURL);
+//   const rawResponse = await getData(popularMoviesURL);
 
-  state.movies = rawResponse.results;
+//   state.movies = rawResponse.results;
 
-  return rawResponse;
-}
+//   return rawResponse;
+// }
 
-async function getTopRatedMovies() {
-    const topRatedURL = getUrl("/movie/top_rated");
+// async function getTopRatedMovies() {
+//     const topRatedURL = getUrl("/movie/top_rated");
   
-    const rawResponse = await getData(topRatedURL);
+//     const rawResponse = await getData(topRatedURL);
   
-    state.movies = rawResponse.results;
+//     state.movies = rawResponse.results;
   
-    return rawResponse;
-  }
+//     return rawResponse;
+//   }
   
-async function getPopularSeries() {
-    const popSeriesURL = getUrl("/tv/popular");
+// async function getPopularSeries() {
+//     const popSeriesURL = getUrl("/tv/popular");
 
-    const rawResponse = await getData(popSeriesURL);
+//     const rawResponse = await getData(popSeriesURL);
 
-    state.movies = rawResponse.results;
+//     state.movies = rawResponse.results;
 
-    return rawResponse;
-}
+//     return rawResponse;
+// }
 
 
-/**
- * Crea una card per i film / serie tv
- */
-function getMovieCard(imgURL, title) {
-  const cardWrap = document.createElement("div");
-  const coverImg = document.createElement("img");
+// /**
+//  * Crea una card per i film / serie tv
+//  */
+// function getMovieCard(imgURL, title) {
+//   const cardWrap = document.createElement("div");
+//   const coverImg = document.createElement("img");
 
-  const textWrap = document.createElement("div");
-  const text = document.createElement("h3");
+//   const textWrap = document.createElement("div");
+//   const text = document.createElement("h3");
   
 
-  cardWrap.classList.add("card");
-  textWrap.classList.add("card__title_wrap");
+//   cardWrap.classList.add("card");
+//   textWrap.classList.add("card__title_wrap");
 
-  text.textContent = title;
-  coverImg.src = imgURL;
+//   text.textContent = title;
+//   coverImg.src = imgURL;
 
-  textWrap.appendChild(text);
-  cardWrap.append(coverImg, textWrap);
+//   textWrap.appendChild(text);
+//   cardWrap.append(coverImg, textWrap);
 
-  cardWrap.addEventListener("click", overlay, {
-    once: true
-  });
+//   cardWrap.addEventListener("click", overlay, {
+//     once: true
+//   });
 
-  return cardWrap;
-}
+//   return cardWrap;
+// }
 
-/**
- * genera le card per i film presenti nel parametro "list"
- * e li appende dentro il nodo parent passato come secondo parametro
- * "sectionNode"
- */
-function renderCarousel(list, sectionNode) {
-  list.forEach((item) => {
-    // ottiene la url dell'immagine completa
-    const imgURL = getImageUrl(item.backdrop_path);
+// /**
+//  * genera le card per i film presenti nel parametro "list"
+//  * e li appende dentro il nodo parent passato come secondo parametro
+//  * "sectionNode"
+//  */
+// function renderCarousel(list, sectionNode) {
+//   list.forEach((item) => {
+//     // ottiene la url dell'immagine completa
+//     const imgURL = getImageUrl(item.backdrop_path);
 
-    const movieCard = getMovieCard(imgURL, item.title);
+//     const movieCard = getMovieCard(imgURL, item.title);
 
-    sectionNode.appendChild(movieCard);
-  });
-}
+//     sectionNode.appendChild(movieCard);
+//   });
+// }
 
-/**
- * funzione che ottiene i dati dall'eseterno,
- * e quando li ha ottenuti renderizza il carosello dei film popolari
- */
-function handleHTMLMounted() {
-  Promise.all([handleSession(), getConfiguration(), getPopularMovies()]).then(
-    () => {
-      // ci permette di lavorare con i dati ottenuti dall'esterno
-      renderCarousel(state.movies, POPULAR_MOVIES);
-    }
-  );
-}
+// /**
+//  * funzione che ottiene i dati dall'eseterno,
+//  * e quando li ha ottenuti renderizza il carosello dei film popolari
+//  */
+// function handleHTMLMounted() {
+//   Promise(getPopularMovies()).then(
+//     () => {
+//       // ci permette di lavorare con i dati ottenuti dall'esterno
+//       renderCarousel(state.movies, POPULAR_MOVIES);
+//     }
+//   );
+// }
 
-function handleHTMLMounted2() {
-    Promise.all([handleSession(), getConfiguration(), getTopRatedMovies()]).then(
-      () => {
-        // ci permette di lavorare con i dati ottenuti dall'esterno
-        renderCarousel(state.movies, TOPRATED_MOVIES);
-      }
-    );
-}
+// function handleHTMLMounted2() {
+//     Promise.all([handleSession(), getConfiguration(), getTopRatedMovies()]).then(
+//       () => {
+//         // ci permette di lavorare con i dati ottenuti dall'esterno
+//         renderCarousel(state.movies, TOPRATED_MOVIES);
+//       }
+//     );
+// }
 
-function handleHTMLMounted3() {
-    Promise.all([handleSession(), getConfiguration(), getPopularSeries()]).then(
-      () => {
-        // ci permette di lavorare con i dati ottenuti dall'esterno
-        renderCarousel(state.movies, POPULAR_SERIES);
-      }
-    );
-}
+// function handleHTMLMounted3() {
+//     Promise.all([handleSession(), getConfiguration(), getPopularSeries()]).then(
+//       () => {
+//         // ci permette di lavorare con i dati ottenuti dall'esterno
+//         renderCarousel(state.movies, POPULAR_SERIES);
+//       }
+//     );
+// }
 
 
-/**
- * listener sul lifecycle "DOMContentLoaded"
- *
- * esegue la funzione handleHTMLMounted appena l'html del nostro
- * index.html è stato stampato a video
- *
- * rimuove il listenr una volta terminata l'operazione con {once: true}
- */
-document.addEventListener("DOMContentLoaded", handleHTMLMounted, {
-  once: true
-});
+// /**
+//  * listener sul lifecycle "DOMContentLoaded"
+//  *
+//  * esegue la funzione handleHTMLMounted appena l'html del nostro
+//  * index.html è stato stampato a video
+//  *
+//  * rimuove il listenr una volta terminata l'operazione con {once: true}
+//  */
+// document.addEventListener("DOMContentLoaded", handleHTMLMounted, {
+//   once: true
+// });
 
-document.addEventListener("DOMContentLoaded", handleHTMLMounted2, {
-    once: true
-});
+// document.addEventListener("DOMContentLoaded", handleHTMLMounted2, {
+//     once: true
+// });
 
-document.addEventListener("DOMContentLoaded", handleHTMLMounted3, {
-    once: true
-});
+// document.addEventListener("DOMContentLoaded", handleHTMLMounted3, {
+//     once: true
+// });
